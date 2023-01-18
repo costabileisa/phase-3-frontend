f54ad113d34cbf9e3d63965890fd514cc48f11c3
Isabella Costabile <costabileisa@gmail.com>
2023-01-18 13:46:20 -0500

    added routes and empty form component

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,35 @@
-import React from 'react';
-import DogCollection from "./DogCollection"
+import React, { useState, useEffect } from 'react';
+import { Switch, Route, useHistory } from "react-router-dom";
+
+import DogCollection from "./DogCollection";
+import DogForm from "./DogForm";
 
 function App() {
+    const [dogs, setDogs] = useState([])
+    const history = useHistory()
+
+    useEffect(() => {
+        fetch("http://localhost:9292/dogs")
+        .then(res => res.json())
+        .then(data => setDogs(data))
+    }, [])
+
+    function addDog() {
+        history.push("/add_dog")
+    }
+
     return (
-        <DogCollection />
+        <div id="App">
+            <Switch>
+                <Route exact path="/">
+                    <button onClick={addDog} style={{float: "right"}}>Add Dog</button>
+                    <DogCollection dogs={dogs} />
+                </Route>
+                <Route path="/add_dog">
+                    <DogForm />
+                </Route>
+            </Switch>
+        </div>
     )
 }
 
--- src/components/DogCollection.js
+++ src/components/DogCollection.js
@@ -1,16 +1,9 @@
-import { useEffect, useState } from 'react';
+import React from 'react';
 import DogCard from "./DogCard"
 
-function DogCollection() {
-    const [dogs, setDogs] = useState([])
+function DogCollection({ dogs }) {
 
-    useEffect(() => {
-        fetch("http://localhost:9292/dogs")
-        .then(res => res.json())
-        .then(data => setDogs(data))
-    }, [])
-
-    const dogsToList = dogs.map(dog => <DogCard dog={dog} />)
+    const dogsToList = dogs.map(dog => <DogCard key={dog.id} dog={dog} />)
 
     return (
         <div id="dog-collection">
