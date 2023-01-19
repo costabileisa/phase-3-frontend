211cc5b0e50e200bdc6c74f0e36b772ca1ff46cc
Isabella Costabile <costabileisa@gmail.com>
2023-01-19 12:04:13 -0500

    'click me' button functionality -> sends user to different route with more info on the dog

--- src/components/DogCard.js
+++ src/components/DogCard.js
@@ -1,15 +1,23 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
 
 function DogCard({ dog }) {
-  return (
-    <div id="card">
-      <div id="card-details">
-        <h2>{dog.name}</h2>
-        <img src={dog.img_url} alt={dog.img_description} className="dog-avatar" />
-        <button>Click Me</button>
-      </div>
-    </div>
-  );
+    const history = useHistory();
+    
+    function handleClick(e) {
+        const id = e.target.parentNode.id;
+        history.push(`/${id}`)
+    }
+
+    return (
+        <div id="card">
+            <div className="card-details" id={dog.id}>
+                <h2>{dog.name}</h2>
+                <img src={dog.img_url} alt={dog.img_description} className="dog-avatar" />
+                <button onClick={handleClick}>Click Me</button>
+            </div>
+        </div>
+    );
 }
 
 export default DogCard;
